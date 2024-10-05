8a14e605dc52e0c961b75563e0f1494d41e00440
Débora Regina Adorni <de.adorni@yahoo.com.br>
2024-10-05 11:02:12 -0300

    Api structure

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,33 @@
 from typing import List
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+import uvicorn
 from fastapi import FastAPI
 
-from model import SeismicDetection
+from api import router
+from config import DATABASE_URL
 
 
 app = FastAPI(debug=True)
+app.include_router(router)
 
 
-database: List[SeismicDetection] = []
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 
 @app.get("/ping")
-def pong():
-    return {"ping": "pong!"}
+def pin():
+    return "pong"
 
