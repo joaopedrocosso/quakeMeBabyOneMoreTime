074a80a153b7db209bc7f3702bd0229fb503e4c8
João Pedro Cosso <cossocodes@gmail.com>
2024-10-05 23:40:43 -0300

    feat: Backend Implementations

---
+++ backend/models.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+from typing import Optional
+from fastapi import UploadFile, File
+
+from pydantic import BaseModel
+
+
+class EventFilter(BaseModel):
+    start_date: Optional[datetime] = None
+    end_date: Optional[datetime] = None
+    station: Optional[str] = None
+
+class UploadEvent(BaseModel):
+    file: UploadFile = File(...)
+    filename: str
+
+class UserSeismicDetectionCreate(BaseModel):
+    filename: str
+
+class Catalog(BaseModel):
+    id: int
+    body: str
+    filename: str
+    arrival_time_abs: Optional[datetime] = None
+    arrival_time_rel: Optional[float] = None
+    evid: Optional[str] = None
+    mq_type: Optional[str] = None
+    network: Optional[str] = None
+    station: Optional[str] = None
+    location: Optional[str] = None
+    channel: Optional[str] = None
+    starttime: Optional[datetime] = None
+    endtime: Optional[datetime] = None
+    sampling_rate: Optional[float] = None
+    delta: Optional[float] = None
+    npts: Optional[int] = None
+    calib: Optional[float] = None
+    _format: Optional[str] = None
+    mseed: Optional[object] = None
+
+    class Config:
+        orm_mode = True
+
+
+class Data(BaseModel):
+    id: int
+    body: str 
+    filename: str
+    time_abs: datetime
+    time_rel: float
+    velocity: float
+    event: str
+
+    class Config:
+        orm_mode = True
+
+class UserEvent(BaseModel):
+    id: int
+    filename: str
+    event: str
+
+    class Config:
+        orm_mode = True
+
+class EventAudio(BaseModel):
+    id: int
+    event_id: int
+    user_event_id: int
+    audio: bytes
+
+    class Config:
+        orm_mode = True
--- backend/repository.py
+++ backend/repository.py
@@ -1,4 +1,44 @@
-from main import get_db
+from config import DATABASE_URL
+import database_models as dbmodels
+import models
+from sqlalchemy import create_engine, and_
+from sqlalchemy.orm import sessionmaker
+
+
+print("Connecting to database...")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        return db
+    finally:
+        db.close()
 
 def get_event_by_id(event_id: int):
-    return get_db.get_event_by_id(event_id)
+    return get_db().query(dbmodels.Catalog).filter(dbmodels.Catalog.id == event_id).first()
+
+
+def list_events(filter: models.EventFilter):
+    filters = []
+    
+    if filter.station:
+        filters.append(dbmodels.Catalog.station == filter.station)
+
+    if filter.start_date:
+        filters.append(dbmodels.Catalog.starttime >= filter.start_date)
+
+    if filter.end_date:
+        filters.append(dbmodels.Catalog.starttime <= filter.end_date)
+
+    query = get_db().query(dbmodels.Catalog)
+
+    if filters:
+        query = query.filter(and_(*filters))
+
+    return query.all()
+
+
+def save_event(event: dbmodels.UserEvent):
+    return get_db().save_event(event)
