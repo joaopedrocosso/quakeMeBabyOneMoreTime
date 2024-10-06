9b54360b6e7ed70a34d5ff05a93c2a11f93a050f
Débora Regina Adorni <de.adorni@yahoo.com.br>
2024-10-06 18:41:46 -0300

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,13 @@ from api import router
 
 app = FastAPI(debug=True)
 app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/ping")
