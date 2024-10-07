2ce339aa5a715a5abff65a0a764876dbf20db730
Bianca <biancaa.barbosa@outlook.com>
2024-10-06 22:55:40 -0300

    feat: ajusted filter

--- frontend/src/components/BottomNavBar.tsx
+++ frontend/src/components/BottomNavBar.tsx
@@ -24,12 +24,7 @@ import { Button } from "./ui/button";
 import NavBarItem from "./NavBarItem";
 import ListItems from "./ListItems";
 
-interface EventsItem {
-    id: number;
-    filename: string;
-    station: string;
-    starttime: string;
-}
+
 import { PlanetTravelCard } from "./PlanetTravelCard";
 import { Checkbox } from "@/components/ui/checkbox"
 import Image from "next/image";
@@ -41,10 +36,20 @@ interface BodyProps {
     body: string | null;
 }
 
+
+interface EventsItem {
+    id: number;
+    filename: string;
+    station: string;
+    starttime: string;
+}
+
+
 export const BottomNavBar = () => {
 
     const [activeButton, setActiveButton] = useState<string | null>(null);
     const [data, setData] = useState([]);
+    const [oldData, setOldData] = useState([]);
     const [station, setStation] = useState("");
     const [dateInitial, setDateInitial] = useState("");
 
@@ -81,6 +86,7 @@ export const BottomNavBar = () => {
             if(!data)
                 throw "Error"
             
+            if(oldData.length === 0) setOldData(data);
             setData(data);
 
         } catch(error) {
@@ -178,7 +184,7 @@ export const BottomNavBar = () => {
                                             </SelectTrigger>
                                             <SelectContent>
                                                 {
-                                                    data.map((item:{starttime: string})=> {
+                                                    oldData.map((item:{starttime: string})=> {
                                                         const newDate = getTimestamp(item.starttime);
                                                         return(
                                                             <SelectItem value={item.starttime}>{newDate}</SelectItem>
